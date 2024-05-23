14184a09965d21d838149b5048452b43ce2e592b
Jack Yu <yiyaoyu@cs.ucla.edu>
2024-05-23 15:13:36 -0700

    Avoid auto pickling of entire monitor by spark, added new callback registration interface for SparkReplayer

--- mobile_insight/monitor/spark/spark_replayer.py
+++ mobile_insight/monitor/spark/spark_replayer.py
@@ -20,12 +20,12 @@ from .submonitor import SparkSubmonitor
 from . import group_by, collect_by
 
 
-def collect(self):
+def _collect(self):
     return self.source.spark_results[self]
 
 
-def _ret_self(x):
-    return x
+def _identity(self):
+    return self
 
 
 class SparkReplayer(OfflineReplayer):
@@ -97,40 +97,116 @@ class SparkReplayer(OfflineReplayer):
         self._output_path = path
 
     def set_group_function(self, func):
+        '''Manually group the packet logs into tasks.
+
+        Task is the minimum partioning unit SparkReplayer uses to dispatch
+        logs to specific analyzers.
+        '''
         self._group_function = func
 
-    def register(self, analyzer, init_args=None, collect_func=None,
-                 export_func=None):
+    def register(self, analyzer, spark_init=None, spark_export=None,
+                 spark_collect=None):
+        '''Register an analyzer.
+
+        This function has been overloaded to support registering special
+        functions/args for SparkReplayer. These special functions/args must
+        have the following properties:
+
+        spark_init - Arguments to initialize an analyzer
+          - Can be either a list of positional arguments used to initialize
+            the analyzer "x = [a, b, c]" -> "MyAnalyzer(*x)"; or a function
+            that returns a list of positional arguments.
+          - If None is specified, the class method "MyAnalyzer.spark_init()"
+            is used. Declare this with the "@classmethod" to avoid errors.
+          - If the class method does not exist, a default value of "[]" is
+            used.
+        spark_export - Export results from workers
+          - Must be a function of signiture "MyAnalyzer -> Any".
+          - If None is specified, the class method
+            "MyAnalyzer.spark_export(analyzer_obj)" is used. Declare this
+            with the "@classmethod" to avoid errors.
+          - If the class method does not exist, the identity function is used.
+        spark_collect - Reduce exported results to a single value
+          - Must be a function of signiture "list[Any] -> Any".
+          - If None is specified, the class method
+            "MyAnalyzer.spark_collect(analyzer_objs)" is used. Declare this
+            with the "@classmethod" to avoid errors.
+          - If the class method does not exist, the identity function is used.
+        '''
         OfflineReplayer.register(self, analyzer)
-        if init_args is None:
-            init_args = []
-        if collect_func is None:
-            collect_func = collect_by.grouped_tasks
-        if export_func is None:
-            export_func = _ret_self
+        if spark_init is None:
+            try:
+                getattr(analyzer, 'spark_init')
+                spark_init = analyzer.__class__.spark_init
+            except AttributeError:
+                spark_init = []
+        if spark_collect is None:
+            try:
+                getattr(analyzer, 'spark_collect')
+                spark_collect = analyzer.__class__.spark_collect
+            except AttributeError:
+                spark_collect = _identity
+        if spark_export is None:
+            try:
+                getattr(analyzer, 'spark_export')
+                spark_export = analyzer.__class__.spark_export
+            except AttributeError:
+                spark_export = _identity
         if analyzer not in self._analyzer_info:
             self._analyzer_info[analyzer] = (id(analyzer), analyzer.__class__,
-                                             init_args, collect_func,
-                                             export_func)
+                                             spark_init, spark_collect,
+                                             spark_export)
             try:
                 getattr(analyzer, 'collect')
             except AttributeError:
                 self.log_info(('%s is a default analyzer, adding "collect()" '
                               'function for data collection')
                               % str(analyzer.__class__))
-                analyzer.collect = collect.__get__(analyzer, analyzer.__class__)
+                analyzer.collect = _collect.__get__(analyzer,
+                                                    analyzer.__class__)
 
-    def set_analyzer_callbacks(self, analyzer, init_args=None,
-                               collect_func=None, export_func=None):
+    def set_analyzer_callbacks(self, analyzer, spark_init=None,
+                               spark_export=None, spark_collect=None):
+        '''Register an analyzer callback
+
+        This should be called if one wants to change the SparkReplayer
+        callback functions after registering the analyzer. These special
+        functions/args must have the following properties:
+
+        spark_init - Arguments to initialize an analyzer
+          - Can be either a list of positional arguments used to initialize
+            the analyzer "x = [a, b, c]" -> "MyAnalyzer(*x)"; or a function
+            that returns a list of positional arguments.
+          - If None is specified, the class method "MyAnalyzer.spark_init()"
+            is used. Declare this with the "@classmethod" to avoid errors.
+          - If the class method does not exist, a default value of "[]" is
+            used.
+        spark_export - Export results from workers
+          - Must be a function of signiture "MyAnalyzer -> Any".
+          - If None is specified, the class method
+            "MyAnalyzer.spark_export(analyzer_obj)" is used. Declare this
+            with the "@classmethod" to avoid errors.
+          - If the class method does not exist, the identity function is used.
+        spark_collect - Reduce exported results to a single value
+          - Must be a function of signiture "list[Any] -> Any".
+          - If None is specified, the class method
+            "MyAnalyzer.spark_collect(analyzer_objs)" is used. Declare this
+            with the "@classmethod" to avoid errors.
+          - If the class method does not exist, the identity function is used.
+        '''
         if analyzer in self._analyzer_info:
             curr = self._analyzer_info[analyzer]
             self._analyzer_info[analyzer] = (
                 id(analyzer),
                 analyzer.__class__,
-                init_args if init_args is not None else curr[2],
-                collect_func if collect_func is not None else curr[3],
-                export_func if export_func is not None else curr[4]
+                spark_init if spark_init is not None else curr[2],
+                spark_collect if spark_collect is not None else curr[3],
+                spark_export if spark_export is not None else curr[4]
             )
+        else:
+            self.log_info(('%s is not a registered analyzer. '
+                           'Ignoring callback registration')
+                          % str(analyzer.__class__))
 
     def deregister(self, analyzer):
         OfflineReplayer.deregister(self, analyzer)
@@ -160,13 +236,21 @@ class SparkReplayer(OfflineReplayer):
                        .load(self._input_path)))
 
         # Decode files
+        args = [
+            os.getcwd(),
+            None,
+            self._output_path,
+            self._sampling_rate,
+            self._type_names,
+            self._skip_decoding,
+        ]
         decoded = (logs.rdd.map(lambda x:
-                                SparkDecoder(os.getcwd(),
+                                SparkDecoder(args[0],
                                              os.path.basename(x.path),
-                                             self._output_path,
-                                             self._sampling_rate,
-                                             self._type_names,
-                                             self._skip_decoding).decode(x))
+                                             args[2],
+                                             args[3],
+                                             args[4],
+                                             args[4]).decode(x))
                    .toDF(decoded_schema)
                    .select('*', posexplode('content'))
                    .drop('content')
@@ -178,11 +262,13 @@ class SparkReplayer(OfflineReplayer):
         decoded.cache().count()
 
         # Partition the data, then launch submonitors and collect results
+        args = [
+            os.getcwd(),
+            list(self._analyzer_info.values()),
+        ]
         results = (self._group_function(decoded).applyInPandas(
-            lambda x: SparkSubmonitor(os.getcwd(),
-                                      list(self._analyzer_info.values()))
-            .run(x), '_ int, obj map<long, binary>')
-                   .drop('_'))
+            lambda x: SparkSubmonitor(*args).run(x),
+            '_ int, obj map<long, binary>').drop('_'))
 
         for analyzer, tup in self._analyzer_info.items():
             analyzer_id, _, _, collect_func, _ = tup
